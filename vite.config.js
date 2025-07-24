40a429a7fdfb553df21c33403e6cbaafe591ab22
brestok <6234513@gmail.com>
2025-07-24 13:23:19 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'https://brestok-1.github.io',
+  base: 'https://brestok-1.github.io/lut-ai-ui',
   build: {
     outDir: 'dist',
   },
